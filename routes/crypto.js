0fc91e73a6cc1779eaa90d1fa0f7407d9cfbef70
Juan Jose <juan.urrego@finelia.fr>
2020-01-31 01:58:17 +0100

    added date retreive from bitfinex

--- routes/crypto.js
+++ routes/crypto.js
@@ -1,8 +1,30 @@
+const https = require('https');
+
 module.exports = {
     getCryptoPage: (req, res) => {
-        res.render('crypto.ejs', {
-            title: "CryptoStat | Crypto",
-            subTitle: "Crypto"
+        let urlBitfinex = "https://api-pub.bitfinex.com/v2/candles/trade:";
+        let urlTemp = "1m:tBTCUSD/hist?limit=100";
+
+        let config = {method: "GET", headers: { accept: "application/json" } };
+
+        https.get(urlBitfinex+urlTemp, config, function (res1) {
+            let data = "";
+
+            res1.on('data', function(content){
+                data+= content;
+            });
+
+            res1.on("end", function () {
+                const result = JSON.parse(data);
+                res.render('crypto.ejs', {
+                    title: "CryptoStat | Crypto",
+                    subTitle: "Crypto",
+                    data: result
+                });
+            });
+
         });
+
+
     }
 };
